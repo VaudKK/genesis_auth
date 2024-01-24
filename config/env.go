542767e5a1af123320569fa9836e5e44432f29ed
Vaud Keith <vkagong@safaricom.co.ke>
2024-01-24 18:27:38 +0300

    initial commit

---
+++ config/env.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var AppConfig  struct {
+	MONGOURI string
+	JWT_KEY string
+}
+
+func LoadConfig() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	AppConfig.MONGOURI = os.Getenv("MONGOURI")
+	AppConfig.JWT_KEY = os.Getenv("JWT_KEY")
+}
