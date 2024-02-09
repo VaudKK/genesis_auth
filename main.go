f6b99e0c4c1655f677ed4d597bd3c21b41d55805
Vaud Keith <vkagong@safaricom.co.ke>
2024-02-09 13:31:19 +0300

    feat: setting collection name env variables

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"genesis_auth/controller"
 	"genesis_auth/routes"
 	"genesis_auth/service"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	authenticationService    service.AuthenticationService       = service.NewAuthenticationService(config.GetCollection(config.DB, "gen_users"))
+	authenticationService    service.AuthenticationService       = service.NewAuthenticationService(config.GetCollection(config.DB, os.Getenv("AUTH_STAGING")))
 	authenticationController controller.AuthenticationController = controller.NewContributionController(authenticationService)
 	authenticationRoutes     routes.AuthenticationRoutes         = routes.NewAuthecationRoutes(authenticationController)
 )
