718f41da42f429eaa885dc7a018eb9c8ab25d48c
Vaud Keith <vkagong@safaricom.co.ke>
2024-02-10 22:42:34 +0300

    fix: moving configs to environment variables

--- config/env.go
+++ config/env.go
@@ -1,24 +1,15 @@
 package config
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
-var AppConfig  struct {
+var AppConfig struct {
 	MONGOURI string
-	JWT_KEY string
+	JWT_KEY  string
 }
 
 func LoadConfig() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	AppConfig.MONGOURI = os.Getenv("MONGOURI")
 	AppConfig.JWT_KEY = os.Getenv("JWT_KEY")
 }
